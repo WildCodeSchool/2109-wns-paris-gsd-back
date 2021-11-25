e449b4d0fc459591d1c95383370eba1b9156d9c3
Rico-la <rico.alves91@gmail.com>
2021-11-25 15:20:05 +0100

    change mutation loginUser into query

--- src/modules/User/User.test.ts
+++ src/modules/User/User.test.ts
@@ -1,45 +1,42 @@
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express'
 import bcrypt from 'bcrypt'
 
-import createServer from "../../server";
-import User from '../../entity/User';
+import createServer from '../../server'
+import User from '../../entity/User'
 
-
-let server: ApolloServer;
+let server: ApolloServer
 
 beforeAll(async () => {
-    server = await createServer();
+  server = await createServer()
 })
 
 describe('login resolver', () => {
-    describe('loginUser mutation', () => {
-        
-        it('logs a user and return a valid json Token', async () => {
+  describe('loginUser mutation', () => {
+    it('logs a user and return a valid json Token', async () => {
+      const user = await User.create({
+        username: 'toto',
+        email: 'toto@toto.com',
+        firstName: 'toto',
+        lastName: 'toto',
+        password: await bcrypt.hash('toto', 10),
+      })
 
-            const user = await User.create({
-                username: 'toto',
-                email: 'toto@toto.com',
-                firstName: 'toto',
-                lastName: 'toto',
-                password: await bcrypt.hash('toto', 10),
-            })
-            
-            await user.save();
+      await user.save()
 
-            const loginUserMutation = gql`
-                mutation LoginUser($data: LoginInput!) {
-                    loginUser(data: $data)
-                }
-            `;
+      const loginUserMutation = gql`
+        query LoginUser($data: LoginInput!) {
+          loginUser(data: $data)
+        }
+      `
 
-            const variables = { data: { username: 'toto', password: 'toto'}};
+      const variables = { data: { username: 'toto', password: 'toto' } }
 
-            const {data} = await server.executeOperation({
-                query: loginUserMutation,
-                variables,
-            })
+      const { data } = await server.executeOperation({
+        query: loginUserMutation,
+        variables,
+      })
 
-            expect(data).toBeTruthy();
-        })
+      expect(data).toBeTruthy()
     })
+  })
 })
