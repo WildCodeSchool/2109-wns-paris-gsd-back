2efa049c0adc1b891e6c94d3b3a2695f1010a6a8
Eurico Da Custodia <rico.alves91@gmail.com>
2022-03-29 10:26:33 +0200

    protected our first route with context

--- src/modules/Role/Role.resolver.ts
+++ src/modules/Role/Role.resolver.ts
@@ -1,4 +1,6 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { ApolloError } from "apollo-server-express";
+import { JwtPayload } from "jsonwebtoken";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 import Role from "../../entity/Role";
 import RoleInput from "./RoleInput/RoleInput";
@@ -6,16 +8,21 @@ import RoleInput from "./RoleInput/RoleInput";
 @Resolver(Role)
 export default class RoleResolver {
   @Query(() => [Role])
-  async getRoles(): Promise<Role[]> {
-    
-    const roles = await Role.find();
-    return roles;
-  }  
+  async getRoles(@Ctx() ctx: JwtPayload): Promise<Role[] | ApolloError> {
+    if (ctx && ctx.payload) {
+      const roles = await Role.find();
+      return roles;
+    }
+
+    return new ApolloError("Not authorized")
+
+
+  }
 
   @Mutation(() => Role)
   async addRole(@Arg("data") data: RoleInput): Promise<Role> {
-    
-    const role = Role.create({...data});
+
+    const role = Role.create({ ...data });
 
     await role.save();
 
