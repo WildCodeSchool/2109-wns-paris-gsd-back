e86a976d40c68e0a23dc4b034d1960084a9519e6
Eurico Da Custodia <rico.alves91@gmail.com>
2022-03-29 10:24:43 +0200

    added context Apollo Server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { ApolloServer } from 'apollo-server-express'
+import jwt, { JsonWebTokenError, JwtPayload, Secret } from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import { buildSchema } from 'type-graphql'
 import TaskResolver from './modules/Task/Task.resolver'
-import CommentResolver from './modules/Comment/Comment.resolver'
 import UserResolver from './modules/User/User.resolver'
+import CommentResolver from './modules/Comment/Comment.resolver'
 import RoleResolver from './modules/Role/Role.resolver'
 import ProjectResolver from './modules/Project/Project.resolver'
+
+dotenv.config()
 
 async function createServer() {
   const schema = await buildSchema({
@@ -14,7 +18,22 @@ async function createServer() {
   const server = new ApolloServer(
     {
       schema,
-      context: ({ req, res }) => ({ req, res }),
+      context: ({ req }) => {
+        const token = req.headers.authorization;
+
+        if (token) {
+          let payload: JwtPayload;
+          try {
+            payload = jwt.verify(token, process.env.JSON_TOKEN_KEY as Secret) as JwtPayload;
+            return { payload };
+          } catch (err) {
+            throw new JsonWebTokenError("Invalid Token");
+
+          }
+        } else {
+          return {}
+        }
+      },
     })
   return server;
 }
