c3cb83c55cb9517af0af3503ee76c7e35a4d4b49
Eurico Da Custodia <rico.alves91@gmail.com>
2022-04-28 10:25:27 +0200

    cette fois ci cest la bonne

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,6 @@ const start = async () => {
   await connectPostgres()
   const server = await createServer()
   console.log(server)
-  console.log(server)
-  console.log(server)
-  console.log(server)
-  console.log(server)
   const app = express()
 
   await server.start()
