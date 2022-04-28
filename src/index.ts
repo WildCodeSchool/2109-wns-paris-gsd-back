fa1d200282b99d8eb10aad547e26691329ce8291
Eurico Da Custodia <rico.alves91@gmail.com>
2022-04-28 10:14:49 +0200

    test 7

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ const start = async () => {
   await connectPostgres()
   const server = await createServer()
   console.log(server)
+  console.log(server)
+  console.log(server)
+  console.log(server)
+  console.log(server)
   const app = express()
 
   await server.start()
