acf4e1580abc9f88a111bf965083a0a2d03d470b
Eurico Da Custodia <rico.alves91@gmail.com>
2022-06-28 15:23:57 +0200

    clean up files and pass all test

--- src/modules/Role/Role.test.ts
+++ src/modules/Role/Role.test.ts
@@ -22,7 +22,6 @@ describe('role resolver', () => {
           }
         }
       `
-
       const adminRole = await Role.findOne({ label: RoleName.ADMIN })
       const adminUser = User.create({
         firstName: "john",
@@ -35,14 +34,14 @@ describe('role resolver', () => {
 
       await adminUser.save();
 
-      const payload = { id: adminUser.id, username: adminUser.username, role: adminUser.role.label }
+      const payload = { id: adminUser.id, username: adminUser.username, role: adminUser.role }
       token = mockToken(payload)
 
 
       const { data, errors } = await server.executeOperation({
         query: getRolesQuery,
       },
-        { req: mockRequest(token) } as ExpressContext
+        { req: { ...mockRequest(token), body: { variables: {} } } } as ExpressContext
       )
 
       expect(!errors).toBeTruthy()
@@ -50,6 +49,40 @@ describe('role resolver', () => {
       const expectedResult = await Role.find()
       data!.getRoles[0].id = +data!.getRoles[0].id
       expect(data!.getRoles[0]).toEqual(expectedResult[0])
+    });
+
+    it('should retrieve an error if user is no ADMIN or MANAGER', async () => {
+      const getRolesQuery = gql`
+        query getRoles {
+          getRoles {
+            id
+            label
+          }
+        }
+      `
+      const developerRole = await Role.findOne({ label: RoleName.DEVELOPER })
+      const developer = User.create({
+        firstName: "john",
+        lastName: "developer",
+        username: "developer",
+        email: "developer@developer.td",
+        password: "lennon",
+        role: developerRole
+      });
+
+      await developer.save();
+
+      const payload = { id: developer.id, username: developer.username, role: developer.role }
+      token = mockToken(payload)
+
+
+      const { errors } = await server.executeOperation({
+        query: getRolesQuery,
+      },
+        { req: { ...mockRequest(token), body: { variables: {} } } } as ExpressContext
+      )
+      expect(errors).toBeTruthy()
+      expect(errors![0].message).toEqual('Access denied! You don\'t have permission for this action!')
     })
   })
 
@@ -82,14 +115,14 @@ describe('role resolver', () => {
 
       await adminUser.save();
 
-      const payload = { id: adminUser.id, username: adminUser.username, role: adminUser.role.label }
+      const payload = { id: adminUser.id, username: adminUser.username, role: adminUser.role }
       token = mockToken(payload)
 
       const { data, errors } = await server.executeOperation({
         query: addRoleMutation,
         variables,
       },
-        { req: mockRequest(token) } as ExpressContext
+        { req: { ...mockRequest(token), body: { variables } } } as ExpressContext
       )
 
       expect(!errors).toBeTruthy()
@@ -99,6 +132,48 @@ describe('role resolver', () => {
       expect(data!.addRole).toEqual(expect.objectContaining({ label: expectedResult!.label }));
 
     })
+    it(`should retrieve an error when the no user ADMIN is attempt to add role`, async () => {
+      await Role.delete({ label: RoleName.USER })
+
+      const addRoleMutation = gql`
+        mutation AddRole($data: RoleInput!) {
+          addRole(data: $data) {
+            label
+          }
+        }
+      `
+      const variables = {
+        data: {
+          label: "USER"
+        }
+      }
+
+      const managerRole = await Role.findOne({ label: RoleName.MANAGER })
+      const managerUser = User.create({
+        firstName: "john",
+        lastName: "manager",
+        username: "manager",
+        email: "manager@test.td",
+        password: "lennon",
+        role: managerRole
+      });
+
+      await managerUser.save();
+
+      const payload = { id: managerUser.id, username: managerUser.username, role: managerUser.role }
+      token = mockToken(payload)
+
+      const { errors } = await server.executeOperation({
+        query: addRoleMutation,
+        variables,
+      },
+        { req: { ...mockRequest(token), body: { variables } } } as ExpressContext
+      )
+
+      expect(errors).toBeTruthy();
+      expect(errors![0].message).toEqual('Access denied! You don\'t have permission for this action!');
+
+    });
   })
 
 })
